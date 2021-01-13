19db3ee94e9bf86f8b5d45efce65c8d600bf78af
Zhijie Zhang <zhijiezhang0124@gmail.com>
2021-01-13 14:53:15 -0800

    Initial commit to set up the project

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './ExploreContainer.css';
+import WebViewer from '@pdftron/webviewer';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.xod',
+      },
+      ref.current as unknown as HTMLElement,
+    ).then((instance) => {
+    });
+  }, [])
+
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div className="container" ref={ref}>
     </div>
   );
 };
